53d548febc49f2696337d4ea10b88827bfb986bd
Chris Sawczuk <chris@sawcz.uk>
2020-08-07 20:22:46 +0100

    Display restaurants on map using hardcoded restaurant list

--- app/src/androidTest/java/com/equalexperts/client/softwaredesignsystems/eatout/EatOutToHelpOutJourneyTest.kt
+++ app/src/androidTest/java/com/equalexperts/client/softwaredesignsystems/eatout/EatOutToHelpOutJourneyTest.kt
@@ -11,13 +11,21 @@ import org.hamcrest.BaseMatcher
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.BoundingBox
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.FolderOverlay
+import org.osmdroid.views.overlay.Marker
 import kotlin.math.abs
 
 class EatOutToHelpOutJourneyTest {
+
+    @Before
+    fun launchApp() {
+        launch(MainActivity::class.java)
+    }
 
     @Test
     fun userAgentWillBeProvidedToOmsdroidLib() {
@@ -29,14 +37,14 @@ class EatOutToHelpOutJourneyTest {
 
     @Test
     fun willDisplayMapUponLaunch() {
-        launch(MainActivity::class.java)
+        launchApp()
 
         onView(withId(R.id.mainMap)).check(matches(isDisplayed()))
     }
 
     @Test
     fun mapDefaultsToLondon() {
-        launch(MainActivity::class.java)
+        launchApp()
 
         onView(withId(R.id.mainMap)).check(
             matches(
@@ -52,9 +60,34 @@ class EatOutToHelpOutJourneyTest {
         )
     }
 
+    @Test
+    fun willDisplayRestaurants() {
+        launchApp()
 
+        onView(withId(R.id.mainMap)).check(matches(hasOverlayWithText("Stub Restaurant for testing purposes")))
+    }
 
-    private fun mapDisplayingBoundingBoxNear(boundingBox: BoundingBox): Matcher<in View>? {
+    private fun hasOverlayWithText(restaurantName: String): BaseMatcher<in View> {
+        return object : BaseMatcher<View?>() {
+            override fun describeTo(description: Description) {
+                description.appendText("map displaying overlay with restaurant $restaurantName")
+            }
+
+            override fun matches(item: Any): Boolean {
+                if (item is MapView) {
+                    item.overlays.forEach {
+                        if (it is FolderOverlay) {
+                            return it.items.any { (it as? Marker)?.title == restaurantName }
+                        }
+                    }
+                }
+                return false
+            }
+        }
+
+    }
+
+    private fun mapDisplayingBoundingBoxNear(boundingBox: BoundingBox): Matcher<in View> {
         return object : BaseMatcher<View?>() {
             override fun describeTo(description: Description) {
                 description.appendText("map displaying bounding box within 0.01 degrees of $boundingBox")
