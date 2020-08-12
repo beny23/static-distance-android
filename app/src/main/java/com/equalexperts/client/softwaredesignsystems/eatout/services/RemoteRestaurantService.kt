bdebbe14f963fbd8e0b7eda943aa815b8999390b
Chris Sawczuk <chris@sawcz.uk>
2020-08-12 20:56:10 +0100

    Move adaption onto request thread

--- app/src/main/java/com/equalexperts/client/softwaredesignsystems/eatout/services/RemoteRestaurantService.kt
+++ app/src/main/java/com/equalexperts/client/softwaredesignsystems/eatout/services/RemoteRestaurantService.kt
@@ -1,16 +1,45 @@
 package com.equalexperts.client.softwaredesignsystems.eatout.services
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import okhttp3.ResponseBody
+import retrofit2.*
 import retrofit2.http.GET
 import retrofit2.http.Path
+import java.lang.reflect.Type
+import java.nio.charset.Charset
+
+class RestaurantConverterFactory : Converter.Factory() {
+    override fun responseBodyConverter(
+        type: Type,
+        annotations: Array<Annotation>,
+        retrofit: Retrofit
+    ): Converter<ResponseBody, *>? {
+        return Converter<ResponseBody, List<Restaurant>> { value ->
+            val restaurantCsv = value.bytes().toString(Charset.defaultCharset())
+            restaurantCsv
+                .split("\n")
+                .drop(1)
+                .filter { !it.isBlank() }
+                .map {
+                    val (name, postcode, lat, lon) = it.split(",")
+                    Restaurant(
+                        name,
+                        postcode,
+                        Location(
+                            lat.toDouble(),
+                            lon.toDouble()
+                        )
+                    )
+                }
+        }
+    }
+}
 
 interface EatOutToHelpOutRestaurantApi {
     @GET("pubgrid/{x}/{path}.csv")
-    fun retrieveRestaurants(@Path("x") x: String, @Path("path") path: String): Call<String>
+    fun retrieveRestaurants(
+        @Path("x") x: String,
+        @Path("path") path: String
+    ): Call<List<Restaurant>>
 }
 
 class RemoteRestaurantService(baseUrl: String) :
@@ -18,7 +47,7 @@ class RemoteRestaurantService(baseUrl: String) :
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(baseUrl)
-        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(RestaurantConverterFactory())
         .build()
 
     private val api = retrofit.create(EatOutToHelpOutRestaurantApi::class.java)
@@ -27,45 +56,31 @@ class RemoteRestaurantService(baseUrl: String) :
         location: Location,
         results: (RestaurantServiceResult) -> Unit
     ) {
-        api.retrieveRestaurants("${location.gridX}", "${location.gridX}-${location.gridY}").enqueue(object :
-            Callback<String?> {
-            override fun onFailure(call: Call<String?>, t: Throwable) {
-                results(RestaurantServiceResult.NetworkError)
-            }
-
-            override fun onResponse(call: Call<String?>, response: Response<String?>) {
-                when {
-                    response.isSuccessful -> parseRestaurantBody(response, results)
-                    else -> results(RestaurantServiceResult.ServerError)
+        api.retrieveRestaurants("${location.gridX}", "${location.gridX}-${location.gridY}")
+            .enqueue(object :
+                Callback<List<Restaurant>?> {
+                override fun onFailure(call: Call<List<Restaurant>?>, t: Throwable) {
+                    results(RestaurantServiceResult.NetworkError)
                 }
-            }
-        })
+
+                override fun onResponse(
+                    call: Call<List<Restaurant>?>,
+                    response: Response<List<Restaurant>?>
+                ) {
+                    when {
+                        response.isSuccessful -> parseRestaurantBody(response, results)
+                        else -> results(RestaurantServiceResult.ServerError)
+                    }
+                }
+            })
     }
 
     private fun parseRestaurantBody(
-        response: Response<String?>,
+        response: Response<List<Restaurant>?>,
         results: (RestaurantServiceResult) -> Unit
     ) {
-        response.body()?.let { restaurantCsv ->
-            results(
-                RestaurantServiceResult.Success(
-                    restaurantCsv
-                        .split("\n")
-                        .drop(1)
-                        .filter { !it.isBlank() }
-                        .map {
-                            val (name, postcode, lat, lon) = it.split(",")
-                            Restaurant(
-                                name,
-                                postcode,
-                                Location(
-                                    lat.toDouble(),
-                                    lon.toDouble()
-                                )
-                            )
-                        })
-            )
+        response.body()?.let { restaurants ->
+            results(RestaurantServiceResult.Success(restaurants))
         }
     }
-
 }
